125103e33984a5ebb4e7ca3c24b594b6832b77ca
Abhishek <alphasierra101at@gmail.com>
2023-05-28 20:35:14 +0530

    some resizing

--- script.js
+++ script.js
@@ -12,13 +12,18 @@ let weather = {
         const { icon, description } = data.weather [0];
         const { temp, humidity} = data.main;
         const { speed } = data.wind;
+
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon + ".png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C" ;
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%" ;
         document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+
+        //removing the dummy text on loading page
         document.querySelector(".weather").classList.remove("loading");
+
+        //loading photos from various cities
         document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "' )"
         
 
@@ -26,17 +31,19 @@ let weather = {
     search: function (){
         this.fetchWeather(document.querySelector(".searchbox").value);
  }
+ };
 
- 
-
- }
  document.querySelector(".search button").addEventListener("click", function (){
    weather.search();
  });
+ 
+ //pressing enter key works here
  document.querySelector(".searchbox").addEventListener("keyup", function(event){
     if (event.key == "Enter"){
         weather.search();
     }
 });
 
+
+//on loading page it call weatherfetch
 weather.fetchWeather("Dehradun");
